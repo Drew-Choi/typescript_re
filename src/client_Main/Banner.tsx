8f5cdce3215206ca47652465d4ecce4530c171d5
Drew <drc920630@gmail.com>
2023-07-08 21:42:38 +0900

    과제 연습 서비스 따라해보기

---
+++ src/client_Main/Banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import bannerStyle from './banner.module.scss'
+
+export default function Banner({position}: {position: number}):JSX.Element {
+  return (
+    <div className={bannerStyle.container}>
+      <p className={bannerStyle.desc}>태블릿이 필요없는<br/> 합리적인 테이블 오더, <br/> 태그히어</p>
+      <div className={bannerStyle.deco}></div>
+      <div className={bannerStyle.img}></div>
+    </div>
+  )
+}
---
+++ src/client_Main/Main.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import mainStyle from './main.module.scss'
+import Banner from './Banner';
+
+export default function Main(): JSX.Element {
+  // 스크롤포지션 저장
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+  // 스크롤 값 인지
+  const scrollHandle = () => {
+    setScrollPosition((cur: number)  => window.scrollY)
+  };
+
+  // 마운트시 스크롤핸들 작동하여 위치 값을 담음
+  useEffect(() => {
+    window.addEventListener('scroll', scrollHandle);
+
+    return () => {
+      window.addEventListener('scroll', scrollHandle);
+    }
+  },[])
+
+  return (
+    <main className={mainStyle.container}>
+      <Banner position={scrollPosition}/>
+    </main>
+  )
+}
