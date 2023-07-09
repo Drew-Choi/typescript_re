8af359bcf8f54af01cb4b7990d9044c8088cdd68
Drew <drc920630@gmail.com>
2023-07-10 02:47:04 +0900

    스크롤이벤트 얼추 완성

--- src/client_Main/Main.tsx
+++ src/client_Main/Main.tsx
@@ -3,14 +3,34 @@ import mainStyle from './main.module.scss';
 import Banner from './Banner';
 import { IconButton } from '@mui/material';
 import MarkChatUnreadIcon from '@mui/icons-material/MarkChatUnread';
-import SecondSection from './SecondSection';
 import BTN from 'BTN_group/BTN';
+import styled from 'styled-components';
+
+const Phone_start = styled.img<{ position?: number }>`
+  position: fixed;
+  display: ${({ position }) => `${(position || 0) > 80 ? 'block' : 'none'}`};
+  left: ${({ position }) =>
+    `${
+      -700 + 10 + (position || 0) >= -60 ? -60 : -700 + 10 + (position || 0)
+    }px`};
+  bottom: ${({ position }) =>
+    `${
+      -600 + 10 + (position || 0) >= 40 ? 40 : -600 + 10 + (position || 0)
+    }px`};
+  z-index: 5;
+  transform: scale(1.2)
+    ${({ position }) =>
+      `${(position || 0) >= 850 ? `rotate(20deg)` : 'rotate(0deg)'}`};
+  transition: 0.2s ease-out;
+`;
 
 export default function Main(): React.JSX.Element {
-  const mainRef = useRef<HTMLInputElement>(null);
+  // const mainRef = useRef<HTMLInputElement>(null);
 
   // 스크롤포지션 저장
   const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+  console.log(scrollPosition);
 
   // 스크롤 값 인지
   const scrollHandle = () => {
@@ -27,30 +47,34 @@ export default function Main(): React.JSX.Element {
     };
   }, []);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver((e) => {
-      console.log(e);
-      e.forEach((el) => {
-        if (el.isIntersecting) {
-          (el.target as HTMLElement).style.opacity = '1';
-        } else {
-          (el.target as HTMLElement).style.opacity = '0';
-        }
-      });
-    });
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver((e) => {
+  //     e.forEach((el) => {
+  //       if (el.isIntersecting) {
+  //         (el.target as HTMLElement).style.opacity = '1';
+  //         (el.target as HTMLElement).style.transition = '1s ease';
+  //       } else {
+  //         (el.target as HTMLElement).style.opacity = '0';
+  //       }
+  //     });
+  //   });
 
-    Array.prototype.forEach.call(mainRef.current?.children, (child) => {
-      observer.observe(child);
-    });
-  }, []);
+  //   Array.prototype.forEach.call(mainRef.current?.children, (child) => {
+  //     observer.observe(child);
+  //   });
+  // }, []);
 
   return (
-    <main className={mainStyle.container} ref={mainRef}>
+    <main className={mainStyle.container}>
       <IconButton className={mainStyle.messageIcon} aria-label="MarkChatUnread">
         <MarkChatUnreadIcon fontSize="inherit" />
       </IconButton>
       <Banner />
-      <SecondSection />
+      <Phone_start
+        position={scrollPosition}
+        src="./phone_hand_locked.webp"
+        alt="phon_start"
+      />
       <div
         className={`${
           scrollPosition > 20 ? mainStyle.bottomMenuOn : mainStyle.bottomMenu
